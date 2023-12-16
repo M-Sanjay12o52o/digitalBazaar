7c5037cce9e8a2d2c3e408878d3f137797f6b443
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-12-16 19:53:27 +0530

    final commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
-        protocol: "http",
-      },
-    ],
+    domains: ["localhost", "digitalbazaar-production.up.railway.app"],
   },
 };
 
